83d983c056e7113250536ed3e1e71a1c54a233a7
Praveen-Pandian <praveenpandian04@gmail.com>
2023-07-22 17:39:30 +0530

    Server update

---
+++ src/auth/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAVPjuJBSnRI31OaDeRIGtvYcgRubdaHFg",
+    authDomain: "bookmyevent-2d1e9.firebaseapp.com",
+    projectId: "bookmyevent-2d1e9",
+    storageBucket: "bookmyevent-2d1e9.appspot.com",
+    messagingSenderId: "67971930501",
+    appId: "1:67971930501:web:343ca5df0c0661fcc98cec"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
